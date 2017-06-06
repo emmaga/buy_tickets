9274c01fe33a964f411df01d2bcd87993f92c802
John-Code <john0927@live.com>
2017-06-06 13:28:35 +0800

    添加登录页面样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,30 +1,76 @@
 <template>
-  <div class="hello">
-    <h2>login</h2>
-    <form @submit.prevent="login" class="form-group">
-      <!--账号-->
-      <label class="control-label" for="account">账号</label> 
-      <p :class="{ 'control': true }">
-        <input required v-validate="'required'" :class="{'form-control': true, 'has-error': errors.has('account') }" v-model="account" name="account" type="text" placeholder="请输入账号">
-      </p>
-      <!--密码-->
-      <label class="control-label" for="password">密码</label> 
-      <p :class="{ 'control': true }">
-        <input required v-validate="'required'" :class="{'form-control': true, 'has-error': errors.has('password') }" v-model="password" name="password" type="password" placeholder="请输入密码">
-      </p>
-      <!--分销商code-->
-      <label class="control-label" for="OTACode">分销商code</label> 
-      <p :class="{ 'control': true }">
-        <input required v-validate="'required'" :class="{'form-control': true, 'has-error': errors.has('OTACode') }" v-model="OTACode" name="OTACode" type="text" placeholder="请输入分销商code">
-      </p>
-      <!--项目code-->
-      <label class="control-label" for="projectName">项目code</label> 
-      <p :class="{ 'control': true }">
-        <input required v-validate="'required'" :class="{'form-control': true, 'has-error': errors.has('projectName') }" v-model="projectName" name="projectName" type="text" placeholder="请输入项目code">
-      </p>
-      <!--登陆按钮-->
-      <button :disabled="logining" type="submit">登录</button>
-    </form>
+  <!-- container -->
+  <div class="hello bg-gray full-screen">
+
+    <!-- header -->
+    <div id="top-nav" class="navbar navbar-inverse navbar-static-top">
+      <div class="container-fluid">
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand">清云购票平台</a>
+          </div>
+      </div>
+    </div>
+    <!-- header -->
+
+    <hr class="break-space-double">
+    <hr class="break-space-double">
+    <hr class="break-space-double">
+    <hr class="break-space-double">
+    <hr class="break-space-double">
+    <hr class="break-space-double">
+
+    <!-- login -->
+    <div class="row">
+      <div class="center-block fixed-width-5">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">登录</h3>
+          </div>
+          <div class="panel-body">
+
+            <form @submit.prevent="login" class="form-group">
+              <!--账号-->
+              <label class="control-label" for="account">账号</label> 
+              <p :class="{ 'control': true }">
+                <input required v-validate="'required'" :class="{'form-control': true, 'has-error': errors.has('account') }" v-model="account" name="account" type="text" placeholder="请输入账号">
+              </p>
+              <!--密码-->
+              <label class="control-label" for="password">密码</label> 
+              <p :class="{ 'control': true }">
+                <input required v-validate="'required'" :class="{'form-control': true, 'has-error': errors.has('password') }" v-model="password" name="password" type="password" placeholder="请输入密码">
+              </p>
+              <!--分销商code-->
+              <label class="control-label" for="OTACode">分销商code</label> 
+              <p :class="{ 'control': true }">
+                <input required v-validate="'required'" :class="{'form-control': true, 'has-error': errors.has('OTACode') }" v-model="OTACode" name="OTACode" type="text" placeholder="请输入分销商code">
+              </p>
+              <!--项目code-->
+              <label class="control-label" for="projectName">项目code</label> 
+              <p :class="{ 'control': true }">
+                <input required v-validate="'required'" :class="{'form-control': true, 'has-error': errors.has('projectName') }" v-model="projectName" name="projectName" type="text" placeholder="请输入项目code">
+              </p>
+              <!--登陆按钮-->
+              <hr class="break-space">
+              <div class="control-group">
+                <button :disabled="logining" type="submit" class="btn btn-primary btn-block">登录</button>
+              </div>
+            </form>
+
+          
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
 </div>
   </div>
 </template>
