6f19ffb0134791aeee3b03509379fa19fd2eb899
Emma <emma_0812@msn.cn>
2017-06-07 00:44:31 +0800

    限制订票游玩日期在范围内

--- src/components/main/MBuyTicket.vue
+++ src/components/main/MBuyTicket.vue
@@ -188,41 +188,6 @@ export default {
   },
   created () {
     that = this
-    $(function () {
-      $('input[name="visitTime"]').daterangepicker({
-        singleDatePicker: true,
-        showDropdowns: true,
-        locale: {
-          "customRangeLabel": "Custom",
-          "daysOfWeek": [
-            "日",
-            "一",
-            "二",
-            "三",
-            "四",
-            "五",
-            "六"
-          ],
-          "monthNames": [
-            "一月",
-            "二月",
-            "三月",
-            "四月",
-            "五月",
-            "六月",
-            "七月",
-            "八月",
-            "九月",
-            "十月",
-            "十一月",
-            "十二月"
-          ]
-        }
-      },
-      function (start, end, label) {
-        this.visitTime = start.format('YYYYMMDDHHmmss')
-      })
-    })
   },
   methods: {
     init () {
@@ -234,6 +199,43 @@ export default {
       this.contactsIdType = 'ID_CARD'
       this.contactsIdNum = ''
       this.travelerList = []
+      $(function () {
+        $('input[name="visitTime"]').daterangepicker({
+          singleDatePicker: true,
+          showDropdowns: true,
+          "minDate": moment(that.$data.tInfo.VisitDateStart).format('MM/DD/YYYY'),
+          "maxDate": moment(that.$data.tInfo.VisitDateEnd).format('MM/DD/YYYY'),
+          locale: {
+            "customRangeLabel": "Custom",
+            "daysOfWeek": [
+              "日",
+              "一",
+              "二",
+              "三",
+              "四",
+              "五",
+              "六"
+            ],
+            "monthNames": [
+              "一月",
+              "二月",
+              "三月",
+              "四月",
+              "五月",
+              "六月",
+              "七月",
+              "八月",
+              "九月",
+              "十月",
+              "十一月",
+              "十二月"
+            ]
+          }
+        },
+        function (start, end, label) {
+          this.visitTime = start.format('YYYYMMDDHHmmss')
+        })
+      })
     },
     close () {
       this.isShow = false
